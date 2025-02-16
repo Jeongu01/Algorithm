c536b49a5f860ad781aa1f7343734a43f287e7e7
KimJeongu <kjw101147@gmail.com>
2025-02-16 21:12:33 +0900

    최소 힙

---
+++ Baekjoon/src/dataStructure/Baekjoon_1927.java
@@ -0,0 +1,33 @@
+package dataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1927 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+
+    PriorityQueue<Integer> queue = new PriorityQueue<>();
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int input = Integer.parseInt(st.nextToken());
+
+      if (input == 0) {
+        if (queue.isEmpty()) {
+          System.out.println(0);
+        } else {
+          System.out.println(queue.poll());
+        }
+      } else {
+        queue.add(input);
+      }
+    }
+  }
+
+}
