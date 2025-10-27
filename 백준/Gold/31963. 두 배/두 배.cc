780a810e7298a073764104a998aa39e98704b07d
KimJeongu <113880965+Jeongu01@users.noreply.github.com>
2025-10-27 21:48:33 +0900

    [Gold III] Title: 두 배, Time: 32 ms, Memory: 2032 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.cc
+++ 백준/Gold/31963. 두 배/두 배.cc
@@ -1,28 +1,26 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	int N;
-	cin >> N;
-	long long pre, cur, ans = 0;
-	cin >> pre;
-	for (int i = 1; i < N; i++)
-	{
-		cin >> cur;
-		if (pre > cur)
-		{
-			while (pre > cur)
-			{
-				cur <<= 1;
-				ans++;
-			}
-		}
-		pre = cur;
-	}
-	cout << ans;
+    int N;
+    cin >> N;
+    int pre, pre_cnt = 0;
+    long long ans = 0;
+    cin >> pre;
+    for (int i = 1; i < N; i++)
+    {
+        int cur, cur_cnt = 0;
+        cin >> cur;
+        cur_cnt = max((int)ceil(log2((double)pre / (double)cur)) + pre_cnt, 0);
+        ans += cur_cnt;
+        pre = cur;
+        pre_cnt = cur_cnt;
+    }
+    cout << ans;
 }
