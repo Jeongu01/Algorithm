e697dc1a383f2a8ed1fa4f38f053a6ea421f6b54
KimJeongu <113880965+Jeongu01@users.noreply.github.com>
2025-11-09 21:31:54 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m, d;
+    cin >> m >> d;
+    if (m < 2) cout << "Before";
+    else if (m > 2) cout << "After";
+    else if (d < 18) cout << "Before";
+    else if (d == 18) cout << "Special";
+    else if (d > 18) cout << "After";
+}
