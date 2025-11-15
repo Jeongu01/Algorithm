3a0bdaa35cb11c9d35380165174ed38ef2c6972f
KimJeongu <113880965+Jeongu01@users.noreply.github.com>
2025-11-15 15:59:26 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 140 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int N;
-int A[1'000'000];
-int dp[1'000'000];
+vector<int> A;
+vector<int> v;
 
 int main()
 {
@@ -14,26 +16,16 @@ int main()
 	cin >> N;
 	for (int i = 0; i < N; i++)
 	{
-		cin >> A[i];
+		int input;
+		cin >> input;
+		A.push_back(input);
 	}
-	int len = 1;
-	dp[0] = A[0];
+	v.push_back(A[0]);
 	for (int i = 1; i < N; i++)
 	{
-		if (dp[len - 1] < A[i])
-		{
-			dp[len] = A[i];
-			len++;
-			continue;
-		}
-		int start = 0, end = len, mid;
-		while (start < end)
-		{
-			mid = (start + end) / 2;
-			if (dp[mid] < A[i]) start = mid + 1;
-			else end = mid;
-		}
-		dp[start] = A[i];
+		auto it = lower_bound(v.begin(), v.end(), A[i]);
+		if (it == v.end()) v.push_back(A[i]);
+		else *it = A[i];
 	}
-	cout << len;
+	cout << v.size();
 }
