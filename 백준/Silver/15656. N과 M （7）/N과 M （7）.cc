434875bcc46007c148aad54181dd8856c90841f4
KimJeongu <113880965+Jeongu01@users.noreply.github.com>
2025-12-25 23:40:11 +0900

    [Silver III] Title: N과 M (7), Time: 428 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int arr[8];
+int p[8];
+
+void func(int depth)
+{
+    if (depth == M)
+    {
+        for (int i = 0; i < M; i++)
+        {
+            cout << p[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        p[depth] = arr[i];
+        func(depth + 1);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+    for (int i = 0; i < N; i++)
+    {
+        p[0] = arr[i];
+        func(1);
+    }
+}
